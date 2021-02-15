c3b6869e931a98db3c55403ed4805aa289335fdf
Koushik Parepalli <41136248+koushikparepalli@users.noreply.github.com>
2021-02-15 14:38:44 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ Calculator library containing basic Math Operations.
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
-
 
